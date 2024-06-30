6416fcdd01f74a6d9ba96d419e44fcd2c7ad42c7
Dannick Bedard <dannickbedard@MacBook-Air-de-Marianne-2.local>
2024-06-30 16:57:35 -0400

    Ajuste border with default nvim setting.
    Begin to add an add content function to feed the window

--- lua/test/window.lua
+++ lua/test/window.lua
@@ -2,13 +2,7 @@ local api = vim.api
 local buf, win
 local position = 0
 
-
-
-local function getBoxPart(position, type)
-  if type = ""  then
-    
-  end
-end
+-- local border = require("test.border")
 
 local function center(str)
   local width = api.nvim_win_get_width(0)
@@ -20,46 +14,29 @@ local function open_window()
   buf = api.nvim_create_buf(false, true)
   local border_buf = api.nvim_create_buf(false, true)
 
-  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-  api.nvim_buf_set_option(buf, 'filetype', 'whid')
+  api.nvim_set_option_value('bufhidden', 'wipe', {buf = buf})
+  api.nvim_set_option_value('filetype', 'whid', {buf = buf})
 
-  local width = api.nvim_get_option("columns")
-  local height = api.nvim_get_option("lines")
+  local width = api.nvim_win_get_width(0)
+  local height = api.nvim_win_get_height(0)
 
   local win_height = math.ceil(height * 0.8 - 4)
   local win_width = math.ceil(width * 0.8)
   local row = math.ceil((height - win_height) / 2 - 1)
   local col = math.ceil((width - win_width) / 2)
 
-  local border_opts = {
-    style = "minimal",
-    relative = "editor",
-    width = win_width + 2,
-    height = win_height + 2,
-    row = row - 1,
-    col = col - 1
-  }
-
   local opts = {
-    style = "minimal",
+    -- style = "minimal",
     relative = "editor",
     width = win_width,
     height = win_height,
     row = row,
-    col = col
+    col = col,
+    border = "rounded",
+    title = "TESTTING test",
+    footer = "TESTTING test"
   }
 
-  local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
-  local middle_line = '║' .. string.rep(' ', win_width) .. '║'
-
-  for i=1, win_height do
-    table.insert(border_lines, middle_line)
-  end
-
-  table.insert(border_lines, '╚' .. string.rep('═', win_width) .. '╝')
-  api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
-
-  local border_win = api.nvim_open_win(border_buf, true, border_opts)
   win = api.nvim_open_win(buf, true, opts)
   api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "'..border_buf)
 
@@ -137,16 +114,36 @@ local function set_mappings()
 
 end
 
-local function whid()
+local function contentOption(opts, key)
+  if opts[key] and opts[key] == true then
+    api.nvim_buf_set_option(buf, key, false)
+  end
+end
+
+local function generateContent(content, opts)
+-- content is array
+  api.nvim_buf_set_lines(buf, 1, 2, false, {center('HEAD~'..position)})
+  api.nvim_buf_set_lines(buf, 2, 3, false, {"testing"})
+
+  api.nvim_buf_set_lines(buf, 4, -1, false, result)
+
+  api.nvim_buf_add_highlight(buf, -1, 'whidSubHeader', 1, 0, -1)
+  contentOption(opts, "modifiable")
+
+end
+
+
+local function window(content, opts)
   position = 0
   open_window()
   set_mappings()
   update_view(0)
   api.nvim_win_set_cursor(win, {4, 0})
+  generateContent(content, opts)
 end
 
 return {
-  whid = whid,
+  window = window,
   update_view = update_view,
   open_file = open_file,
   move_cursor = move_cursor,
