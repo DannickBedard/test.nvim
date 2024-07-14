11128d819efe47a1f1100b1c3f5af329e4a8c956
Dannick Bedard <dannickbedard@MacBook-Air-de-Marianne-2.local>
2024-07-13 20:48:26 -0400

    ajuste border file strucs

--- lua/test/border.lua
+++ lua/test/border.lua
@@ -1,105 +1,29 @@
-local CORNER_LEFT_TOP = "leftCornerTop"
-local CORNER_RIGHT_TOP = "rightCornerTop"
-local CORNER_LEFT_TOP_ROUNDED = "leftCornerTopRounded"
-local CORNER_RIGHT_TOP_ROUNDED = "rightCornerTopRounded"
-local CORNER_LEFT_BOTTOM = "leftCornerBottom"
-local CORNER_RIGHT_BOTTOM = "rightCornerBottom"
-local CORNER_LEFT_BOTTOM_ROUNDED = "leftCornerBottomRounded"
-local CORNER_RIGHT_BOTTOM_ROUNDED = "rightCornerBottomRounded"
-local SIDE = "side"
-local SIDE_BOTTOM = "sideBottom"
-local SPLIT_TOP = "split_top"
-local SPLIT_BOTTOM = "splitBottom"
-local SPLIT_LEFT_SIDE = "splitLeftSide"
-local SPLIT_RIGHT_SIDE = "splitRightSide"
-local SPLIT_MIDLE = "splitMidle"
 
-local double_border = { }
-double_border[CORNER_LEFT_TOP] = "╔"
-double_border[CORNER_RIGHT_TOP] = "╗"
-double_border[CORNER_LEFT_BOTTOM] = "╚"
-double_border[CORNER_RIGHT_BOTTOM] = "╝"
-double_border[SIDE] = "║"
-double_border[SIDE_BOTTOM] = "═"
-double_border[SPLIT_TOP] = "═"
-double_border[SPLIT_BOTTOM] = "═"
-double_border[SPLIT_MIDLE] = "╬"
-double_border[SPLIT_LEFT_SIDE] = "╠"
-double_border[SPLIT_RIGHT_SIDE] = "╣"
+local constant = require("test.borders.constant")
 
-local simple_border = { }
-simple_border[CORNER_LEFT_TOP] = "┌"
-simple_border[CORNER_RIGHT_TOP] = "┐"
-simple_border[CORNER_LEFT_BOTTOM] = "└"
-simple_border[CORNER_RIGHT_BOTTOM] = "┘"
-simple_border[SIDE] = "│"
-simple_border[SIDE_BOTTOM] = "─"
-simple_border[SPLIT_TOP] = "┬"
-simple_border[SPLIT_BOTTOM] = "┴"
-simple_border[SPLIT_MIDLE] = "┼"
-simple_border[SPLIT_LEFT_SIDE] = "├"
-simple_border[SPLIT_RIGHT_SIDE] = "┤"
-simple_border[CORNER_LEFT_TOP_ROUNDED] = "╭"
-simple_border[CORNER_RIGHT_TOP_ROUNDED] = "╮"
-simple_border[CORNER_RIGHT_BOTTOM_ROUNDED] = "╯"
-simple_border[CORNER_LEFT_BOTTOM_ROUNDED] = "╰"
-
-local simple_thick_border = { }
-simple_thick_border[CORNER_LEFT_TOP] = "┏"
-simple_thick_border[CORNER_RIGHT_TOP] = "┓"
-simple_thick_border[CORNER_LEFT_BOTTOM] = "┗"
-simple_thick_border[CORNER_RIGHT_BOTTOM] = "┛"
-simple_thick_border[SIDE] = "┃"
-simple_thick_border[SIDE_BOTTOM] = "━"
-simple_thick_border[SPLIT_TOP] = "┳"
-simple_thick_border[SPLIT_BOTTOM] = "┻"
-simple_thick_border[SPLIT_MIDLE] = "╋"
-simple_thick_border[SPLIT_LEFT_SIDE] = "┣"
-simple_thick_border[SPLIT_RIGHT_SIDE] = "┫"
-
-function countOccurrences(str, char)
-    local count = 0
-    for i = 1, #str do
-        if string.sub(str, i, i) == char then
-            count = count + 1
-        end
-    end
-    return count
-end
-local stringLen = function(text)
-  local currentLenght = string.len(text)
-  if (string.find(text, ':')) then
-    return currentLenght -- + countOccurrences(text, ':')
-  end
-  return currentLenght
-end
-
+-- TODO :: Handle the case where the width is smaller than the content
 local insertTextContent = function(text, currentBorder, win_width)
+  text = text:gsub("\t", "    ") -- Remove tab char and remplace by 4 space
   text = " " .. text
-  local content = text .. string.rep(' ', win_width - (stringLen(text)))
+  local content = text .. string.rep(' ', win_width - (string.len(text)))
 
-  if string.len(content) > win_width then
-    print("ho nooo...")
-    content = content .. "0"
-  end
-
-  local contentLine =  currentBorder[SIDE] .. content .. currentBorder[SIDE]
+  local contentLine =  currentBorder[constant.SIDE] .. content .. currentBorder[constant.SIDE]
   return contentLine
 end
 
 local insertEmptyContent = function(currentBorder, win_width)
-  local contentLine =  currentBorder[SIDE] .. string.rep(' ', win_width) .. currentBorder[SIDE]
+  local contentLine =  currentBorder[constant.SIDE] .. string.rep(' ', win_width) .. currentBorder[constant.SIDE]
   return contentLine;
 end
 
 
 local insertTextInsideTopBorder = function(text, currentBorder, win_width)
-  local top_border =  currentBorder[CORNER_LEFT_TOP] .. text .. string.rep(currentBorder[SIDE_BOTTOM], win_width - (stringLen(text))) .. currentBorder[CORNER_RIGHT_TOP] 
+  local top_border =  currentBorder[constant.CORNER_LEFT_TOP] .. text .. string.rep(currentBorder[constant.SIDE_BOTTOM], win_width - (string.len(text))) .. currentBorder[constant.CORNER_RIGHT_TOP] 
   return top_border
 end
 
 local insertEmptyTopBorder = function(currentBorder, win_width)
-  local top_border =  currentBorder[CORNER_LEFT_TOP] .. string.rep(currentBorder[SIDE_BOTTOM], win_width) .. currentBorder[CORNER_RIGHT_TOP] 
+  local top_border =  currentBorder[constant.CORNER_LEFT_TOP] .. string.rep(currentBorder[constant.SIDE_BOTTOM], win_width) .. currentBorder[constant.CORNER_RIGHT_TOP] 
   return top_border
 end
 
@@ -108,29 +32,30 @@ local insertTextInsideBottomBorder = function()
 end
 
 local insertEmptyBottomBorder = function(currentBorder, win_width)
-  local bottom_border =  currentBorder[CORNER_LEFT_BOTTOM] .. string.rep(currentBorder[SIDE_BOTTOM], win_width) .. currentBorder[CORNER_RIGHT_BOTTOM] 
+  local bottom_border =  currentBorder[constant.CORNER_LEFT_BOTTOM] .. string.rep(currentBorder[constant.SIDE_BOTTOM], win_width) .. currentBorder[constant.CORNER_RIGHT_BOTTOM] 
   return bottom_border
 end
 
+
 return {
-  doubleBorder = double_border,
-  simpleBorder = simple_border,
-  simpleThickBorder = simple_thick_border,
-  CORNER_LEFT_TOP =  CORNER_LEFT_TOP ,
-  CORNER_RIGHT_TOP =  CORNER_RIGHT_TOP ,
-  CORNER_LEFT_TOP_ROUNDED =  CORNER_LEFT_TOP_ROUNDED ,
-  CORNER_RIGHT_TOP_ROUNDED =  CORNER_RIGHT_TOP_ROUNDED ,
-  CORNER_LEFT_BOTTOM =  CORNER_LEFT_BOTTOM ,
-  CORNER_RIGHT_BOTTOM =  CORNER_RIGHT_BOTTOM ,
-  CORNER_LEFT_BOTTOM_ROUNDED =  CORNER_LEFT_BOTTOM_ROUNDED ,
-  CORNER_RIGHT_BOTTOM_ROUNDED =  CORNER_RIGHT_BOTTOM_ROUNDED ,
-  SIDE =  SIDE ,
-  SIDE_BOTTOM =  SIDE_BOTTOM ,
-  SPLIT_TOP =  SPLIT_TOP ,
-  SPLIT_BOTTOM =  SPLIT_BOTTOM ,
-  SPLIT_LEFT_SIDE =  SPLIT_LEFT_SIDE ,
-  SPLIT_RIGHT_SIDE =  SPLIT_RIGHT_SIDE ,
-  SPLIT_MIDLE =  SPLIT_MIDLE,
+  doubleBorder = require("test.borders.double"),
+  simpleBorder = require("test.borders.simple"),
+  simpleThickBorder = require("test.borders.thick"),
+  CORNER_LEFT_TOP = constant.CORNER_LEFT_TOP ,
+  CORNER_RIGHT_TOP = constant.CORNER_RIGHT_TOP ,
+  CORNER_LEFT_TOP_ROUNDED = constant.CORNER_LEFT_TOP_ROUNDED ,
+  CORNER_RIGHT_TOP_ROUNDED = constant.CORNER_RIGHT_TOP_ROUNDED ,
+  CORNER_LEFT_BOTTOM = constant.CORNER_LEFT_BOTTOM ,
+  CORNER_RIGHT_BOTTOM = constant.CORNER_RIGHT_BOTTOM ,
+  CORNER_LEFT_BOTTOM_ROUNDED = constant.CORNER_LEFT_BOTTOM_ROUNDED ,
+  CORNER_RIGHT_BOTTOM_ROUNDED = constant.CORNER_RIGHT_BOTTOM_ROUNDED ,
+  SIDE = constant.SIDE ,
+  SIDE_BOTTOM = constant.SIDE_BOTTOM ,
+  SPLIT_TOP = constant.SPLIT_TOP ,
+  SPLIT_BOTTOM = constant.SPLIT_BOTTOM ,
+  SPLIT_LEFT_SIDE = constant.SPLIT_LEFT_SIDE ,
+  SPLIT_RIGHT_SIDE = constant.SPLIT_RIGHT_SIDE ,
+  SPLIT_MIDLE = constant.SPLIT_MIDLE,
   fn = {
     topBorder = insertEmptyTopBorder,
     topBorderText = insertTextInsideTopBorder,
